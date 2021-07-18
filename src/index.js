03a05dcfba6bc0f9d3e68c1c21a9a7ce8825d667
Maryna Vereshchak <vereschak12@gmail.com>
2021-07-18 12:43:51 +0300

    create template.hbs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './sass/main.scss';
 import getRefs from './js/get-refs';
 import ApiService from './js/apiService';
+import imageTemplate from './templates/imageTemplate';
 import '@pnotify/core/dist/BrightTheme.css';
 import '@pnotify/core/dist/PNotify.css';
 const { error } = require('@pnotify/core');
